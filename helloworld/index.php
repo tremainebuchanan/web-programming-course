df762885d45cfaf271528a6e156bc22cdc479932
Tremaine Buchanan <tremainekbuchanan@gmail.com>
2020-06-07 09:11:25 -0500

    feat:form controls

--- helloworld/index.php
+++ helloworld/index.php
@@ -2,4 +2,7 @@
     echo "Hello World<br>";
     echo "This is my first PHP Web Application";
     echo "<p>This is a HTML paragraph</p>";
+    echo "<input type='text' placeholder='Placeholder Text'><br>";
+    echo "<textarea></textarea><br>";
+    echo "<select><option>Select Country</option><option>Jamaica</option></select>";
 ?>
