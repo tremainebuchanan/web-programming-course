55172c7d7b6e6dd9f2eb5d17ba3cfc2c4989ea1a
Tremaine Buchanan <tremainekbuchanan@gmail.com>
2020-06-07 09:16:11 -0500

    feat:list items

--- helloworld/index.php
+++ helloworld/index.php
@@ -5,4 +5,6 @@
     echo "<input type='text' placeholder='Placeholder Text'><br>";
     echo "<textarea></textarea><br>";
     echo "<select><option>Select Country</option><option>Jamaica</option></select>";
+    echo "<h2>Records</h2>";
+    echo "<ul><li>First Record</li><li>Second Record</li></ul>";
 ?>
