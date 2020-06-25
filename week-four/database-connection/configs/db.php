b2e2b0f83b50b3ab61e12b167a1903c7096d79e1
bryton danvers <brytondanvers@gmail.com>
2020-06-24 19:43:56 -0500

    updated config

--- week-four/database-connection/configs/db.php
+++ week-four/database-connection/configs/db.php
@@ -2,8 +2,8 @@
 return [
     'database' => [
         'host' => '127.0.0.1',
-        'name' => 'test',
-        'user' => 'root',
+        'name' => 'foo',
+        'user' => 'admin',
         'password' => ''
     ]
 ];
